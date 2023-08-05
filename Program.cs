f604cd2265b3578e9fd166c4cd849a2c1abe2065
Guilherme Nascimento de Farias <guiguidefarias@hotmail.com>
2023-08-05 17:17:09 -0300

    Consultando atraves do Dapper

--- Program.cs
+++ Program.cs
@@ -1,8 +1,13 @@
 ﻿using Microsoft.Data.SqlClient;
+using Dapper;
 
 const string connectionString = "Server=localhost,1433;Database=balta;User Id=sa;Password=@Teste123@;TrustServerCertificate=True";
 
 using (var connection = new SqlConnection(connectionString))
 {
-    
+    var categories = connection.Query<Category>("SELECT [Id], [Title] FROM [Category]");
+    foreach (var category in categories)
+    {
+        Console.WriteLine($"{category.Id} -- {category.Title}");
+    }
 }
